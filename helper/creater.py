3d8bbb5af485351aef9cda6165ffcb2329b48a12
Dewan <iit2015097@iiita.ac.in>
2017-02-23 23:19:14 +0530

    organized code

--- helper/creater.py
+++ helper/creater.py
@@ -1,17 +1,21 @@
-import csv
+# import csv
 import random
 import string
 
 class randomWriter:
     """ Creates random boys and girls """
 
-    def makeBoys():
-        with open('boys.csv', 'w') as csvfile:
+    def __init__(self, no_boys, no_girls):
+        self.boys_no = no_boys
+        self.girls_no = no_girls
+
+    def makeBoys(self):
+        with open('../data/boys.csv', 'w') as csvfile:
             fieldnames = ['name', 'attractiveness', 'intelligence', 'budget', 'is_committed', 'b_type']
             bwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
             bwriter.writeheader()
             b_type_choices = ['miser', 'generous', 'geeks']
-            for i in range(10):
+            for i in range(self.boys_no):
                 temp_obj = {
                     'name': random.choice(string.ascii_uppercase) + ''.join(random.choice(string.ascii_lowercase) for _ in range(5)),
                     'attractiveness': random.randint(0,100),
@@ -23,14 +27,14 @@ class randomWriter:
                 print(temp_obj)
                 bwriter.writerow(temp_obj)
 
-    def makeGirls():
-        with open('girls.csv', 'w') as csvfile:
+    def makeGirls(self):
+        with open('../data/girls.csv', 'w') as csvfile:
             fieldnames = ['name', 'attractiveness', 'intelligence', 'maintainance', 'is_committed', 'g_type', 'choose_type']
             gwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
             gwriter.writeheader()
             choose_type_choices = ['m_attr', 'm_gen', 'm_rich']
-            g_type_choices = ['miser', 'generous', 'geeks']
-            for i in range(20):
+            g_type_choices = ['choosy', 'normal', 'desperate']
+            for i in range(self.girls_no):
                 temp_obj = {
                     'name': random.choice(string.ascii_uppercase) + ''.join(random.choice(string.ascii_lowercase) for _ in range(5)),
                     'attractiveness': random.randint(0,100),
--- test.py
+++ test.py
@@ -1,8 +1,20 @@
-from create_random import randomWriter
+# from create_random import randomWriter
 import os
 import sys
 
-sys.path.append(os.path.abspath('./'))
+sys.path.append(os.path.abspath('./helper/'))
 
-rWriter = randomWriter
-rWriter.makeBoys()
+# print(sys.path)
+
+
+# rWriter = randomWriter(10, 20)
+# rWriter.makeBoys()
+# rWriter.makeGirls()
+
+####################
+
+from reader import Reader
+
+r1 = Reader()
+arr = Reader().readT('b')
+print(arr)

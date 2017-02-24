da13f6cbe7fbf180f46772840fb2fdee94441829
Dewan <iit2015097@iiita.ac.in>
2017-02-24 21:57:48 +0530

    gifts added

---
+++ helper/all_gifts.py
@@ -0,0 +1,21 @@
+class gift_essential :
+	def __init__(self) :
+		self.price = None # int
+		self.value = None # int
+		
+
+class gift_utility :
+	def __init__(self) :
+		self.utility_value = None # int
+		self.utility_class = None # string
+		self.price = None # int
+		self.value = None # int
+		
+
+class gift_luxury :
+	def __init__(self) :
+		self.luxury_rating = None # int
+		self.difficulty = None # int
+		self.price = None # int
+		self.value = None # int
+		
--- helper/creater.py
+++ helper/creater.py
@@ -5,9 +5,10 @@ import string
 class randomWriter:
     """ Creates random boys and girls """
 
-    def __init__(self, no_boys, no_girls):
+    def __init__(self, no_boys, no_girls, no_gifts):
         self.boys_no = no_boys
         self.girls_no = no_girls
+        self.gift_no = no_gifts
 
     def makeBoys(self):
         with open('./data/boys.csv', 'w') as csvfile:
@@ -19,7 +20,7 @@ class randomWriter:
                 temp_obj = {
                     'name': random.choice(string.ascii_uppercase) + ''.join(random.choice(string.ascii_lowercase) for _ in range(5)),
                     'attractiveness': random.randint(0,100),
-                    'min_attr': random.randint(50,100),
+                    'min_attr': random.randint(0,50),
                     'intelligence': random.randint(0,100),
                     'budget': random.randint(0,100),
                     'b_type': random.choice(b_type_choices),
@@ -40,10 +41,50 @@ class randomWriter:
                     'name': random.choice(string.ascii_uppercase) + ''.join(random.choice(string.ascii_lowercase) for _ in range(5)),
                     'attractiveness': random.randint(0,100),
                     'intelligence': random.randint(0,100),
-                    'maintainance': random.randint(0,100),
+                    'maintainance': random.randint(0,50),
                     'g_type': random.choice(g_type_choices),
                     'choose_type':random.choice(choose_type_choices),
                     'is_committed': False
                 }
                 print(temp_obj)
                 gwriter.writerow(temp_obj)
+    
+    def makeGifts(self):
+        with open('./data/gifts.csv', 'w') as csvfile:
+            fieldnames = ['type', 'value', 'price', 'utility_value', 'utility_class', 'luxury_rating', 'difficult']
+            gftwriter = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            gftwriter.writeheader()
+            for i in range(0, self.gift_no):
+                decd = random.randint(0, 10)
+                if(decd <= 4):
+                    # make essential gift
+                    temp_obj = {
+                        'type': 'essential', 
+                        'value': random.randint(0,20), 
+                        'price': random.randint(0,20), 
+                        'utility_value': None, 
+                        'utility_class': None, 
+                        'luxury_rating': None, 
+                        'difficult': None
+                    }
+                elif(decd > 4 and decd <=8 ):
+                    temp_obj = {
+                        'type': 'utility', 
+                        'value': random.randint(0,20), 
+                        'price': random.randint(0,20), 
+                        'utility_value': random.randint(0,10), 
+                        'utility_class': random.randint(0,10), 
+                        'luxury_rating': None, 
+                        'difficult': None
+                    }
+                else:
+                    temp_obj = {
+                        'type': 'luxury', 
+                        'value': random.randint(0,20), 
+                        'price': random.randint(0,20), 
+                        'utility_value': None, 
+                        'utility_class': None, 
+                        'luxury_rating':random.randint(0,10), 
+                        'difficult':random.randint(0,10)
+                    }
+                gftwriter.writerow(temp_obj)

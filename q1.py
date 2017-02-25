094f1260026391327710661a074c39a3924d2a97
Dewan <iit2015097@iiita.ac.in>
2017-02-25 11:11:39 +0530

    q1 ready

---
+++ q1.py
@@ -0,0 +1,22 @@
+import os
+import sys
+sys.path.append(os.path.abspath('./helper/'))
+from reader import ReaderC
+from couple import couple_maker
+from weiter import write_couple
+
+rdr = ReaderC()
+arrB = rdr.readT('b')
+arrG = rdr.readT('g')
+
+cpm = couple_maker()
+logger = write_couple()
+
+arrCPL = []
+for a_girl in arrG:
+    newCPL = cpm.jodi_bana(a_girl, arrB)
+    if newCPL != None:
+        arrCPL.append(newCPL)
+        logger.log(newCPL.boy_name, newCPL.girl_name)
+
+# Result of Q1 is in ./data/couples.csv
